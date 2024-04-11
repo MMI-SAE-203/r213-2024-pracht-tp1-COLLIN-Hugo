39ee2e4ad94b21821056bf871db374d37abba955
COLLIN-Hugo <hugo.collin@edu.univ-fcomte.fr>
2024-04-11 13:49:47 +0200

    copier html / ref

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue';
+
+const maVar = ref(1)
+console.log('maVar : ', maVar.value)
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+  <button @pointerdown="maVar = true?0:1">Montrer 1</button>
+  <h1 v-show="maVar === 1 class="text-2xl">Contenu de 1</h1>
+
+  <button @pointerdown="maVar = 2">Montrer 2</button>
+  <h1 v-show="maVar === 2" class="text-2xl">Contenu de 2</h1>
 </template>
