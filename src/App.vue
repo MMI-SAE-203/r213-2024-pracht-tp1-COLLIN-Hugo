ca9e0c332671dc2a32ac794d20688bbbab0372e0
COLLIN-Hugo <hugo.collin@edu.univ-fcomte.fr>
2024-04-11 14:34:22 +0200

    accordeon fini

--- src/App.vue
+++ src/App.vue
@@ -14,17 +14,14 @@ const menuIsOpen = ref(false)
   aria-controls="mainNav"
   aria-expanded="true"
   class="rounded-full border-2 border-red-600 bg-red-300 px-2"
-  @pointerdown="menuIsOpen = !menuIsOpen"
-  >
-  
+  @pointerdown="menuIsOpen = !menuIsOpen">
   menu
 </button>
 <Transition
   class="transition-transform duration-1000"
   enter-from-class="-translate-x-full"
   enter-to-class="translate-x-0"
-  leave-active-class="-translate-x-full"
->
+  leave-active-class="-translate-x-full">
 <nav id="mainNav" v-show="menuIsOpen">
   <ul>
     <li><a href="#">item 1</a></li>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,21 +8,15 @@ const sectionOpen = ref(1)
 
 <template>
   <section>
-    <button class="text-xl">bouton 1</button>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates sit ad incidunt nisi commodi, exercitationem hic accusantium? Tempore magnam laborum quod error rerum, aut ipsum! Ratione velit magnam accusamus numquam?</p>
+    <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 1?0:1">bouton 1</button>
+    <p v-show="sectionOpen === 1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates sit ad incidunt nisi commodi, exercitationem hic accusantium? Tempore magnam laborum quod error rerum, aut ipsum! Ratione velit magnam accusamus numquam?</p>
   </section>
   <section>
-    <button class="text-xl">bouton 2</button>
-    <p>Facilis nisi corrupti obcaecati pariatur quis? Repellendus iste, nam provident pariatur laudantium laboriosam et maiores fugiat, blanditiis, rerum velit. Quo quis reprehenderit eligendi hic sed a, aspernatur natus similique illum!</p>
+    <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 2?0:2">bouton 2</button>
+    <p v-show="sectionOpen === 2">Facilis nisi corrupti obcaecati pariatur quis? Repellendus iste, nam provident pariatur laudantium laboriosam et maiores fugiat, blanditiis, rerum velit. Quo quis reprehenderit eligendi hic sed a, aspernatur natus similique illum!</p>
   </section>
   <section>
-    <button class="text-xl">bouton 3</button>
-    <p>Assumenda facilis repellendus iure? Assumenda dolores modi aliquid rem esse, itaque corporis harum dolorum nisi delectus velit excepturi dicta. Quibusdam temporibus non assumenda impedit distinctio doloremque vel consectetur inventore optio?</p>
+    <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 3?0:3">bouton 3</button>
+    <p v-show="sectionOpen === 3">Assumenda facilis repellendus iure? Assumenda dolores modi aliquid rem esse, itaque corporis harum dolorum nisi delectus velit excepturi dicta. Quibusdam temporibus non assumenda impedit distinctio doloremque vel consectetur inventore optio?</p>
   </section>
 </template>
-
-<button @pointerdown="maVar = true?0:1">Montrer 1</button>
-<h1 v-show="maVar === 1" class="text-2xl">Contenu de 1</h1>
-
-<button @pointerdown="maVar = 2">Montrer 2</button>
-<h1 v-show="maVar === 2" class="text-2xl">Contenu de 2</h1>
